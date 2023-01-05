8de70966681ba827eea2598876fd00374b488fa0
Hải Lê Văn <vanhaiit2018@gmail.com>
2023-01-05 21:20:23 +0700

    test commit 2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 //Comment thu test git
+// comment thu 2
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
